1965008451edf624810325d6dc69139cae1ee286
Kogsey <KadenKogsey@gmail.com>
2023-07-09 22:18:12 +0800

    Sync

--- Assets/Scripts/IntroText.cs
+++ Assets/Scripts/IntroText.cs
@@ -5,24 +5,32 @@ using TMPro;
 
 public class IntroText : MonoBehaviour
 {
-    [TextArea] [SerializeField] private string[] itemInfo;
-    [SerializeField] private float textSpeed = 0.01f;
+	[TextArea][SerializeField] private string[] itemInfo;
+	[SerializeField] private float textSpeed = 0.01f;
 
-    [Header("UI Elements")]
-    [SerializeField] private TextMeshProUGUI itemInfoText;
-    private int currentlyDisplayingText = 0;
+	[Header("UI Elements")]
+	[SerializeField] private TextMeshProUGUI itemInfoText;
+	private readonly int currentlyDisplayingText = 0;
 
-    public void ActivateText()
-    {
-        StartCoroutine(AnimateText());
-    }
+	public GameObject PlayButton;
 
-    IEnumerator AnimateText()
-    {
-        for (int i = 0; i < itemInfo[currentlyDisplayingText].Length + 1; i++)
-        {
-            itemInfoText.text = itemInfo[currentlyDisplayingText].Substring(0, i);
-            yield return new WaitForSeconds(textSpeed);
-        }
-    }
+	public void ActivateText()
+	{
+		PlayButton.SetActive(false);
+		StartCoroutine(AnimateText());
+	}
+
+	private IEnumerator AnimateText()
+	{
+		for (int i = 0; i < itemInfo[currentlyDisplayingText].Length + 1; i++)
+		{
+			if (Input.GetKeyDown(Settings.CurrentSettings.Jump) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Mouse0))
+				PlayButton.SetActive(true);
+
+			itemInfoText.text = itemInfo[currentlyDisplayingText][..i];
+			yield return new WaitForSeconds(textSpeed);
+		}
+
+		PlayButton.SetActive(true);
+	}
 }
